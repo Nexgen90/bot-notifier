a37424b90d60009152647bb1615af6fbee5c64b1
nikolay.mikutskiy <nikolay.mikutskiy@nexign.com>
2020-07-15 15:13:19 +0300

    Fix UserName

--- src/main/java/ru/nexgen/botnotifier/services/handlers/HelloCallbackHandler.java
+++ src/main/java/ru/nexgen/botnotifier/services/handlers/HelloCallbackHandler.java
@@ -27,17 +27,23 @@ public class HelloCallbackHandler implements MessageHandler {
     @Override
     public void handle(Update receivedMessage) {
         String playerCommand = receivedMessage.getCallbackQuery().getData();
-        log.info("Received player callback: " + playerCommand);
+        log.info("Received player " + receivedMessage.getCallbackQuery().getFrom().getId() + " callback: " + playerCommand);
 
         String[] command = playerCommand.split(":");
         int answerId = Integer.parseInt(command[1]);
 
+        String tgUserName = receivedMessage.getCallbackQuery().getFrom().getUserName();
+        if (tgUserName == null) {
+            tgUserName = receivedMessage.getCallbackQuery().getFrom().getFirstName();
+        } else {
+            tgUserName = "@" + tgUserName;
+        }
         switch (answerId) {
             case 1:
-                msgSender.send("@" + receivedMessage.getCallbackQuery().getFrom().getUserName() + " peek <b>" + answerId + "</b> value!", receivedMessage.getCallbackQuery().getMessage().getChatId());
+                msgSender.send(tgUserName + " peek <b>" + answerId + "</b> value!", receivedMessage.getCallbackQuery().getMessage().getChatId());
                 break;
             case 2:
-                msgSender.send("@" + receivedMessage.getCallbackQuery().getFrom().getUserName() + " peek <b>" + answerId + "</b> value!", receivedMessage.getCallbackQuery().getMessage().getChatId());
+                msgSender.send(tgUserName + " peek <b>" + answerId + "</b> value!", receivedMessage.getCallbackQuery().getMessage().getChatId());
                 break;
             case 3:
                 msgSender.editInLineButton(receivedMessage, "<i>Отменено</i>");
