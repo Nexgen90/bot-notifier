26278e8feef7a664bd15a9d17af7ce9929e6016e
nikolay.mikutskiy <nikolay.mikutskiy@nexign.com>
2020-08-31 17:00:45 +0300

    multi map files loader

--- src/main/java/ru/nexgen/botnotifier/services/map/MapLoader.java
+++ src/main/java/ru/nexgen/botnotifier/services/map/MapLoader.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,15 +24,34 @@ import java.util.Map;
 @Service
 public class MapLoader {
 
+    private String mapsFolderPath = "/Users/nikolay.mikutskiy/git/bot-notifier/src/main/resources/game/map";
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @PostConstruct
     public void init() throws IOException {
+        File folder = new File(mapsFolderPath);
+        List<File> files = listFilesForFolder(folder);
+        Map<Integer, Location> map = new HashMap<>();
 
-        File file = new File("/Users/nikolay.mikutskiy/git/bot-notifier/src/main/resources/game/map/map.json");
-        Map<Integer, Location> map = objectMapper.readValue(file, new TypeReference<HashMap<Integer, Location>>() {
+        files.forEach(f -> {
+            try {
+                map.putAll(objectMapper.readValue(f, new TypeReference<HashMap<Integer, Location>>() {}));
+            } catch (IOException e) {
+                log.error("Error while load map file: {}", f.getName(), e);
+            }
         });
+
         log.info("Map is loaded: {} locations", map.size());
     }
 
+    public List<File> listFilesForFolder(final File folder) {
+        List<File> mapFiles = new ArrayList<>();
+        for (final File fileEntry : folder.listFiles()) {
+            if (!fileEntry.isDirectory()) {
+                log.info("Found map file: {}", fileEntry.getName());
+                mapFiles.add(fileEntry);
+            }
+        }
+        return mapFiles;
+    }
 }
